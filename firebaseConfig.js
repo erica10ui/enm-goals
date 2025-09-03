790d5618cd96ac7f864da41992f30167ccb8c320
erica10ui <ericamanguilimotan@gmail.com>
2025-09-04 00:10:10 +0800

    create goals

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,15 +1,21 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// firebaseConfig.js
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBR2cMHoBezG86Y6niwR97xUuO-pjrhkOU",
+  apiKey: "AIzaSy...",
   authDomain: "enm-goals.firebaseapp.com",
   projectId: "enm-goals",
-  storageBucket: "enm-goals.firebasestorage.app",
-  messagingSenderId: "43719589883",
-  appId: "1:43719589883:web:68c708773729ab4560155e",
-  measurementId: "G-95W28QD85Q"
+  storageBucket: "enm-goals.appspot.com",
+  messagingSenderId: "103774048000",
+  appId: "1:103774048000:web:...",
+  measurementId: "G-LHCK5KNEG0",
 };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Initialize app only once
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+
+// Firestore instance
+const db = getFirestore(app);
+
+export { db };
